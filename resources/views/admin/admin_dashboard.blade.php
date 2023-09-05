60ca75a9f7a42f02b86ae4eb872aa1c9a53191c9
rawan-mortaja <rawanmortaja077@gmail.com>
2023-09-05 16:25:44 +0300

    Admin Profile & Image Update

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -23,6 +23,8 @@
     <!-- Theme Style CSS -->
     <link rel="stylesheet" href="{{ asset('adminbackend/assets/css/dark-theme.css') }}" />
     <link rel="stylesheet" href="{{ asset('adminbackend/assets/css/semi-dark.css') }}" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
     <link rel="stylesheet" href="{{ asset('adminbackend/assets/css/header-colors.css') }}" />
     <title>Rukada - Responsive Bootstrap 5 Admin Template</title>
 </head>
