6e75262c3a79bd56831ed9c124e2698df4f4c248
rawan-mortaja <rawanmortaja077@gmail.com>
2023-09-05 13:50:22 +0300

    Admin login & logout & setup Templete

--- routes/web.php
+++ routes/web.php
@@ -28,19 +28,26 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 });
 
-    Route::middleware(['auth','role:admin'])->group(function() {
-        Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])
+//ِAdmin Dashboard
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])
         ->name('admin.dashboard');
 
-    });
-    Route::middleware(['auth','role:vendor'])->group(function() {
-        Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])
-        ->name('vendor.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])
+        ->name('admin.logout');
+});
 
-    });
+
+//Vendor Dashboard
+Route::middleware(['auth', 'role:vendor'])->group(function () {
+    Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])
+        ->name('vendor.dashboard');
+});
+
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
 
 // Route::middleware(['auth','role:user'])->group(function() {
 //     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])
