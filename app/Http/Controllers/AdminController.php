7dc1e3935937e995c7199e480731422f040802b6
rawan-mortaja <rawanmortaja077@gmail.com>
2023-09-05 20:39:21 +0300

    Admin Profile Change Password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -62,5 +63,31 @@ class AdminController extends Controller
         );
 
         return redirect()->back()->with($notification);
+    }
+
+    public function AdminChangePassword()
+    {
+        return view('admin.admin_change_password');
+    }
+
+    public function AdminUpdatePassword(Request $request)
+    {
+        // Validation
+        $request->validate([
+            'old_password' => ['required'],
+            'new_password' => ['required','confirmed'],
+        ]);
+
+        // Match the old password
+        if (!Hash::check($request->old_password, auth::user()->password)) {
+            return back()->with("error", "Old Password Doesn't Match!!");
+        }
+
+        // Update new password
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password),
+        ]);
+
+        return back()->with("status", "Password Changed Successfully");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,12 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 
     Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])
         ->name('admin.profile.store');
+
+    Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])
+        ->name('admin.change.password');
+
+    Route::post('/admin/update/password', [AdminController::class, 'AdminUpdatePassword'])
+        ->name('update.password');
 });
 
 
