5d3504492e4d5974ed4f6ca69a9a11facf5bcaa8
rawan-mortaja <rawanmortaja077@gmail.com>
2023-09-14 00:57:30 +0300

    4. Subcategory Crud

--- resources/views/backend/subcategory/subcategory_all.blade.php
+++ resources/views/backend/subcategory/subcategory_all.blade.php
@@ -3,20 +3,20 @@
     <div class="page-content">
         <!--breadcrumb-->
         <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-            <div class="breadcrumb-title pe-3">All Category</div>
+            <div class="breadcrumb-title pe-3">All SubCategory</div>
             <div class="ps-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0 p-0">
                         <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}"><i class="bx bx-home-alt"></i></a>
                         </li>
-                        <li class="breadcrumb-item active" aria-current="page">All Category</li>
+                        <li class="breadcrumb-item active" aria-current="page">All SubCategory</li>
                     </ol>
                 </nav>
             </div>
             <div class="ms-auto">
                 <div class="btn-group">
-                    <a href="{{ route('add.category') }}" class="btn btn-primary">
-                        Add Category
+                    <a href="{{ route('add.subcategory') }}" class="btn btn-primary">
+                        Add SubCategory
                     </a>
                     {{-- <button type="button" class="btn btn-primary">Add Brand</button> --}}
                 </div>
@@ -33,19 +33,19 @@
                             <tr>
                                 <th>Sl</th>
                                 <th>Category Name</th>
-                                <th>Category Image</th>
+                                <th>SubCategory Name</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($categories as $key => $item)
+                            @foreach ($subcategories as $key => $item)
                                 <tr>
                                     <td> {{ $key + 1 }}</td>
-                                    <td>{{ $item->category_name }}</td>
-                                    <td><img src="{{ asset($item->category_image) }}" style="width: 70px ; height: 40px;"></td>
+                                    <td>{{ $item->Category->category_name }}</td>
+                                    <td>{{ $item->subcategory_name }}</td>
                                     <td>
-                                        <a href="{{ route('edit.category', $item->id) }}" class="btn btn-info">Edit</a>
-                                        <a href="{{ route('delete.category', $item->id) }} " id="delete"
+                                        <a href="{{ route('edit.subcategory', $item->id) }}" class="btn btn-info">Edit</a>
+                                        <a href="{{ route('delete.subcategory', $item->id) }} " id="delete"
                                             class="btn btn-danger">Delete</a>
                                     </td>
                                 </tr>
@@ -55,7 +55,7 @@
                             <tr>
                                 <th>Sl</th>
                                 <th>Category Name</th>
-                                <th>Category Image</th>
+                                <th>SubCategory Name</th>
                                 <th>Action</th>
                             </tr>
                         </tfoot>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VendorController;
@@ -104,6 +105,26 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 
         Route::get('/delete/category/{id}', 'DeleteCategory')
             ->name('delete.category');
+    });
+
+    Route::controller(SubCategoryController::class)->group(function () {
+        Route::get('/all/subcategory', 'AllSubCategory')
+            ->name('all.subcategory');
+
+        Route::get('/add/subcategory', 'AddSubCategory')
+            ->name('add.subcategory');
+
+        Route::post('/store/subcategory', 'StoreSubCategory')
+            ->name('store.subcategory');
+
+        Route::get('/edit/subcategory/{id}', 'EditSubCategory')
+            ->name('edit.subcategory');
+
+        Route::post('/update/subcategory', 'UpdateSubCategory')
+            ->name('update.subcategory');
+
+        Route::get('/delete/subcategory/{id}', 'DeleteSubCategory')
+            ->name('delete.subcategory');
     });
 });
 
